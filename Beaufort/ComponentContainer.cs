86dfc953ed1e893fa31ad47b11e091d2f216efcb
GraemeB <grae22@gmail.com>
2017-06-03 14:24:41 +0200

    Adding ComponentContainer.

---
+++ Beaufort/ComponentContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Beaufort
+{
+  public class ComponentContainer
+  {
+    //-------------------------------------------------------------------------
+
+    Dictionary<string, IComponent> Components = new Dictionary<string, IComponent>();
+
+    //-------------------------------------------------------------------------
+
+    //-------------------------------------------------------------------------
+  }
+}
---
+++ Beaufort_Test/ComponentContainer_Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Beaufort;
+
+namespace Beaufort_Test
+{
+  [TestFixture]
+  [Category( "ComponentContainer" )]
+  public class ComponentContainer_Test
+  {
+    //-------------------------------------------------------------------------
+
+    ComponentContainer TestObject;
+
+    //-------------------------------------------------------------------------
+
+    [SetUp]
+    public void SetUp()
+    {
+      TestObject = new ComponentContainer();
+    }
+    
+
+    //-------------------------------------------------------------------------
+
+    [Test]
+    public void AddComponent()
+    {
+
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
